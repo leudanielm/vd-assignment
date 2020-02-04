980a7f79275c631e7b5713853fbdf03d67f0bd4f
Daniel Leu <leudanielm@gmail.com>
2020-02-04 21:25:18 +0100

    Move the episodes list in its own component

--- src/ShowDetails.js
+++ src/ShowDetails.js
@@ -2,14 +2,9 @@ import React from 'react';
 import { inject, observer } from 'mobx-react';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import CardMedia from '@material-ui/core/CardMedia';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import IconButton from '@material-ui/core/IconButton';
-import ArrowIcon from '@material-ui/icons/ArrowForward'
 import { compose } from 'recompose';
+
+import ShowEpisodes from './ShowEpisodes';
 
 const styles = ({
   root: {
@@ -32,19 +27,6 @@ const styles = ({
   },
   description: {
     paddingLeft: 10,
-  },
-  episodesContainer: {
-  },
-  card: {
-    width: '100%',
-    margin: 10,
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  cardActions: {
-    justifyContent: 'flex-end',
   },
 });
 
@@ -71,26 +53,7 @@ class ShowDetails extends React.Component {
         }} className={classes.description}></span>
       </div>
       <Typography className={classes.title}>Episodes:</Typography>
-      {showEpisodes.data && <div className={classes.episodesContainer}>
-        {showEpisodes.data.map(episode => episode.image && (
-          <Card className={classes.card}>
-            <CardHeader title={episode.name} />
-            <CardMedia className={classes.media} image={episode.image.medium} />
-            <CardContent>
-              <Typography variant="body2" color="textSecondary" component="p">
-                <span dangerouslySetInnerHTML={{
-                  __html: episode.summary
-                }}></span>
-              </Typography>
-            </CardContent>
-            <CardActions className={classes.cardActions} disableSpacing>
-              <IconButton aria-label="see details">
-                <ArrowIcon />
-              </IconButton>
-            </CardActions>
-          </Card>
-        ))}
-      </div>}
+      {showEpisodes.data && <ShowEpisodes episodes={showEpisodes.data} />}
     </div>);
   }
 }
---
+++ src/ShowEpisodes.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import CardMedia from '@material-ui/core/CardMedia';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowIcon from '@material-ui/icons/ArrowForward'
+
+const styles = ({
+  episodesContainer: {
+  },
+  card: {
+    width: '100%',
+    margin: 10,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  cardActions: {
+    justifyContent: 'flex-end',
+  },
+});
+
+function ShowEpisodes({ classes, episodes }) {
+  return (
+    <div className={classes.episodesContainer}>
+      {episodes.map(episode => episode.image && (
+        <Card key={episode.id} className={classes.card}>
+          <CardHeader title={episode.name} />
+          <CardMedia className={classes.media} image={episode.image.medium} />
+          <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+              <span dangerouslySetInnerHTML={{
+                __html: episode.summary
+              }}></span>
+            </Typography>
+          </CardContent>
+          <CardActions className={classes.cardActions} disableSpacing>
+            <IconButton aria-label="see details">
+              <ArrowIcon />
+            </IconButton>
+          </CardActions>
+        </Card>
+      ))}
+    </div>
+  )
+}
+
+export default withStyles(styles)(ShowEpisodes);
